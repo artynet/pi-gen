3d37bd4d2db99fccfdd73f24049c04dc9ebf8c25
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-10-27 11:41:06 +0700

    Try to fix Java error

--- stage2.5/01-susi/01-run-chroot.sh
+++ stage2.5/01-susi/01-run-chroot.sh
@@ -3,6 +3,10 @@
 sudo -u pi mkdir /home/pi/SUSI.AI
 cd /home/pi/SUSI.AI && sudo -u pi git clone --single-branch https://github.com/fossasia/susi_linux.git
 
+# Purge Java, try to fix "the trustAnchors parameter must be non-empty" error. Ref: https://askubuntu.com/a/1006748/5871
+apt-get purge -y openjdk* java-common ca-certificates-java
+
+# Run SUSI Linux's install script
 cd /home/pi/SUSI.AI/susi_linux && sudo -Hu pi ./install.sh
 
 # After ./install.sh, the /dev is occupied by some processes and cannot be released by build script.
