99ee46405a6218d2a22b4215ad991bd613337762
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-11-20 16:14:08 +0700

    Try fixing "fatal: reference is not a tree"

--- stage2.5/01-susi/01-run-chroot.sh
+++ stage2.5/01-susi/01-run-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo -u pi mkdir /home/pi/SUSI.AI
-cd /home/pi/SUSI.AI && sudo -u pi git clone --single-branch https://github.com/fossasia/susi_linux.git
+cd /home/pi/SUSI.AI && sudo -u pi git clone https://github.com/fossasia/susi_linux.git
 
 # Purge Java, try to fix "the trustAnchors parameter must be non-empty" error. Ref: https://askubuntu.com/a/1006748/5871
 apt-get purge -y openjdk* java-common ca-certificates-java
