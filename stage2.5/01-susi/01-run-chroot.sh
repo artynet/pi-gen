8c79c2cbece6f156d196ed4bf3bf7e8a5667e963
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2018-11-20 11:54:09 +0700

    Build a specific revision of SUSI

--- stage2.5/01-susi/01-run-chroot.sh
+++ stage2.5/01-susi/01-run-chroot.sh
@@ -7,7 +7,7 @@ cd /home/pi/SUSI.AI && sudo -u pi git clone --single-branch https://github.com/f
 apt-get purge -y openjdk* java-common ca-certificates-java
 
 # Run SUSI Linux's install script
-cd /home/pi/SUSI.AI/susi_linux && sudo -Hu pi ./install.sh
+cd /home/pi/SUSI.AI/susi_linux && git checkout "$SUSI_REVISION" && sudo -Hu pi ./install.sh
 
 # After ./install.sh, the /dev is occupied by some processes and cannot be released by build script.
 # We stop those processes to let the build script continue.
