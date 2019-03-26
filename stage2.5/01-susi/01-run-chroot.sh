e0500c2f7bc27f10efbbd4a73cd07b66a5aadde6
Norbert Preining <norbert@preining.info>
2019-03-26 14:51:38 +0900

    Revert "allow building the image on qemu" - moved to susi_linux
    This reverts commit ab8e61ccb0075e4483ea2a3ce2e714a98cd8680c.

--- stage2.5/01-susi/01-run-chroot.sh
+++ stage2.5/01-susi/01-run-chroot.sh
@@ -7,12 +7,7 @@ cd /home/pi/SUSI.AI && sudo -u pi git clone https://github.com/fossasia/susi_lin
 apt-get purge -y openjdk* java-common ca-certificates-java
 
 # Run SUSI Linux's install script
-# add a change to the susi linux install script to 
-# change the seeed-voicecard install script to ignore build machine
-# to allow building in chroot
-cd /home/pi/SUSI.AI/susi_linux && git checkout "$SUSI_REVISION" && \
-	sed -i -e 's/cd seeed-voicecard/cd seeed-voicecard\nsed -i -e "s%^is_Raspberry=.*%is_Raspberry=Raspberry%" install.sh/' install.sh && \
-	sudo -Hu pi ./install.sh
+cd /home/pi/SUSI.AI/susi_linux && git checkout "$SUSI_REVISION" && sudo -Hu pi ./install.sh
 
 # After ./install.sh, the /dev is occupied by some processes and cannot be released by build script.
 # We stop those processes to let the build script continue.
