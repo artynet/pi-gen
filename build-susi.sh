abf914c5efc2c8fc2aa751fe2da80a4dfd3cebb6
Norbert Preining <norbert@preining.info>
2019-04-23 12:16:41 +0900

    move apt-get call into stage2.4 (not repeated on quick rebuild)

--- build-susi.sh
+++ build-susi.sh
@@ -25,14 +25,14 @@ echo -e "SUSI_PULL_REQUEST=$SUSI_PULL_REQUEST" >> config
 
 # Tell pi-gen to skip the phases which are to build desktop image
 touch ./stage3/SKIP ./stage4/SKIP ./stage5/SKIP
-touch ./stage2/SKIP_IMAGES ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
+touch ./stage2/SKIP_IMAGES ./stage2.4/SKIP_IMAGES ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
 # Delete old builds
 sudo rm -rf "$(realpath "$PWD"/../Work/stage2.5)"
 sudo rm -rf "$(realpath "$PWD"/../Work/export-image)"
 sudo rm -rf "$(realpath "$PWD"/../Deploy)"
 # If $FULL_BUILD is not set and the Work/stage2/rootfs folder exists, we do incremental build, based on Raspbian Lite (stage2).
 if [ -z "$FULL_BUILD" ] && [ -d "$(realpath "$PWD"/../Work/stage2/rootfs)" ]; then
-	touch ./stage0/SKIP ./stage1/SKIP ./stage2/SKIP
+	touch ./stage0/SKIP ./stage1/SKIP ./stage2/SKIP ./stage2.4/SKIP
 else 
 	sudo rm -rf "$(realpath "$PWD"/../Work)"
 fi
